e3322b194b7eea297e86a7414c6b8eff63a0b305
Siva <Siva@SivaKumar>
2021-03-26 20:17:16 +0530

    javaproject added only java programs purpose

---
+++ javaprojects/src/javaprojects/JavaTypecastingDemo1.java
@@ -0,0 +1,13 @@
+package javaprojects;
+
+public class JavaTypecastingDemo1 {
+
+	public static void main(String[] args) {
+float f=15.6f;
+//int a=f; //Compail time error
+int a=(int)f;
+System.out.println(f);
+System.out.println(a);
+	}
+
+}
